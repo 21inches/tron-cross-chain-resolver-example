dd157e8b9885df394d83cf0d2ef69effb1451b74
xana-rahmani <xanarahmani@gmail.com>
2025-08-02 02:54:01 +0330

    Update contracts to tron

--- contracts/src/Resolver.sol
+++ contracts/src/Resolver.sol
@@ -2,19 +2,17 @@
 
 pragma solidity 0.8.23;
 
-import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
-
-import {IOrderMixin} from "limit-order-protocol/contracts/interfaces/IOrderMixin.sol";
-import {TakerTraits} from "limit-order-protocol/contracts/libraries/TakerTraitsLib.sol";
-
-import {IResolverExample} from "../lib/cross-chain-swap/contracts/interfaces/IResolverExample.sol";
-import {RevertReasonForwarder} from "solidity-utils/contracts/libraries/RevertReasonForwarder.sol";
-import {IEscrowFactory} from "../lib/cross-chain-swap/contracts/interfaces/IEscrowFactory.sol";
-import {IBaseEscrow} from "../lib/cross-chain-swap/contracts/interfaces/IBaseEscrow.sol";
-import {TimelocksLib, Timelocks} from "../lib/cross-chain-swap/contracts/libraries/TimelocksLib.sol";
-import {Address} from "solidity-utils/contracts/libraries/AddressLib.sol";
-import {IEscrow} from "../lib/cross-chain-swap/contracts/interfaces/IEscrow.sol";
-import {ImmutablesLib} from "../lib/cross-chain-swap/contracts/libraries/ImmutablesLib.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IOrderMixin} from "contracts/interfaces/IOrderMixin.sol";
+import {TakerTraits} from "contracts/libraries/TakerTraitsLib.sol";
+import {IResolverExample} from "contracts/interfaces/IResolverExample.sol";
+import {RevertReasonForwarder} from "@1inch/solidity-utils/contracts/libraries/RevertReasonForwarder.sol";
+import {IEscrowFactory} from "contracts/interfaces/IEscrowFactory.sol";
+import {IBaseEscrow} from "contracts/interfaces/IBaseEscrow.sol";
+import {TimelocksLib, Timelocks} from "contracts/libraries/TimelocksLib.sol";
+import {Address} from "@1inch/solidity-utils/contracts/libraries/AddressLib.sol";
+import {IEscrow} from "contracts/interfaces/IEscrow.sol";
+import {ImmutablesLib} from "contracts/libraries/ImmutablesLib.sol";
 
 /**
  * @title Sample implementation of a Resolver contract for cross-chain swap.
@@ -25,74 +23,74 @@ import {ImmutablesLib} from "../lib/cross-chain-swap/contracts/libraries/Immutab
  * @custom:security-contact security@1inch.io
  */
 contract Resolver is Ownable {
-    using ImmutablesLib for IBaseEscrow.Immutables;
-    using TimelocksLib for Timelocks;
+	using ImmutablesLib for IBaseEscrow.Immutables;
+	using TimelocksLib for Timelocks;
 
-    error InvalidLength();
-    error LengthMismatch();
+	error InvalidLength();
+	error LengthMismatch();
 
-    IEscrowFactory private immutable _FACTORY;
-    IOrderMixin private immutable _LOP;
+	IEscrowFactory private immutable _FACTORY;
+	IOrderMixin private immutable _LOP;
 
-    constructor(IEscrowFactory factory, IOrderMixin lop, address initialOwner) Ownable(initialOwner) {
-        _FACTORY = factory;
-        _LOP = lop;
-    }
+	constructor(IEscrowFactory factory, IOrderMixin lop, address initialOwner) Ownable(initialOwner) {
+		_FACTORY = factory;
+		_LOP = lop;
+	}
 
-    receive() external payable {} // solhint-disable-line no-empty-blocks
+	receive() external payable {} // solhint-disable-line no-empty-blocks
 
-    /**
-     * @notice See {IResolverExample-deploySrc}.
+	/**
+	 * @notice See {IResolverExample-deploySrc}.
      */
-    function deploySrc(
-        IBaseEscrow.Immutables calldata immutables,
-        IOrderMixin.Order calldata order,
-        bytes32 r,
-        bytes32 vs,
-        uint256 amount,
-        TakerTraits takerTraits,
-        bytes calldata args
-    ) external payable onlyOwner {
+	function deploySrc(
+		IBaseEscrow.Immutables calldata immutables,
+		IOrderMixin.Order calldata order,
+		bytes32 r,
+		bytes32 vs,
+		uint256 amount,
+		TakerTraits takerTraits,
+		bytes calldata args
+	) external payable onlyOwner {
 
-        IBaseEscrow.Immutables memory immutablesMem = immutables;
-        immutablesMem.timelocks = TimelocksLib.setDeployedAt(immutables.timelocks, block.timestamp);
-        address computed = _FACTORY.addressOfEscrowSrc(immutablesMem);
+		IBaseEscrow.Immutables memory immutablesMem = immutables;
+		immutablesMem.timelocks = TimelocksLib.setDeployedAt(immutables.timelocks, block.timestamp);
+		address computed = _FACTORY.addressOfEscrowSrc(immutablesMem);
 
-        (bool success,) = address(computed).call{value: immutablesMem.safetyDeposit}("");
-        if (!success) revert IBaseEscrow.NativeTokenSendingFailure();
+		(bool success,) = address(computed).call{value: immutablesMem.safetyDeposit}("");
+		if (!success) revert IBaseEscrow.NativeTokenSendingFailure();
 
-        // _ARGS_HAS_TARGET = 1 << 251
-        takerTraits = TakerTraits.wrap(TakerTraits.unwrap(takerTraits) | uint256(1 << 251));
-        bytes memory argsMem = abi.encodePacked(computed, args);
-        _LOP.fillOrderArgs(order, r, vs, amount, takerTraits, argsMem);
-    }
+		// _ARGS_HAS_TARGET = 1 << 251
+		takerTraits = TakerTraits.wrap(TakerTraits.unwrap(takerTraits) | uint256(1 << 251));
+		bytes memory argsMem = abi.encodePacked(computed, args);
+		_LOP.fillOrderArgs(order, r, vs, amount, takerTraits, argsMem);
+	}
 
-    /**
-     * @notice See {IResolverExample-deployDst}.
+	/**
+	 * @notice See {IResolverExample-deployDst}.
      */
-    function deployDst(IBaseEscrow.Immutables calldata dstImmutables, uint256 srcCancellationTimestamp) external onlyOwner payable {
-        _FACTORY.createDstEscrow{value: msg.value}(dstImmutables, srcCancellationTimestamp);
-    }
+	function deployDst(IBaseEscrow.Immutables calldata dstImmutables, uint256 srcCancellationTimestamp) external onlyOwner payable {
+		_FACTORY.createDstEscrow{value: msg.value}(dstImmutables, srcCancellationTimestamp);
+	}
 
-    function withdraw(IEscrow escrow, bytes32 secret, IBaseEscrow.Immutables calldata immutables) external {
-        escrow.withdraw(secret, immutables);
-    }
+	function withdraw(IEscrow escrow, bytes32 secret, IBaseEscrow.Immutables calldata immutables) external {
+		escrow.withdraw(secret, immutables);
+	}
 
 
-    function cancel(IEscrow escrow, IBaseEscrow.Immutables calldata immutables) external {
-        escrow.cancel(immutables);
-    }
+	function cancel(IEscrow escrow, IBaseEscrow.Immutables calldata immutables) external {
+		escrow.cancel(immutables);
+	}
 
-    /**
-     * @notice See {IResolverExample-arbitraryCalls}.
+	/**
+	 * @notice See {IResolverExample-arbitraryCalls}.
      */
-    function arbitraryCalls(address[] calldata targets, bytes[] calldata arguments) external onlyOwner {
-        uint256 length = targets.length;
-        if (targets.length != arguments.length) revert LengthMismatch();
-        for (uint256 i = 0; i < length; ++i) {
-            // solhint-disable-next-line avoid-low-level-calls
-            (bool success,) = targets[i].call(arguments[i]);
-            if (!success) RevertReasonForwarder.reRevert();
-        }
-    }
+	function arbitraryCalls(address[] calldata targets, bytes[] calldata arguments) external onlyOwner {
+		uint256 length = targets.length;
+		if (targets.length != arguments.length) revert LengthMismatch();
+		for (uint256 i = 0; i < length; ++i) {
+			// solhint-disable-next-line avoid-low-level-calls
+			(bool success,) = targets[i].call(arguments[i]);
+			if (!success) RevertReasonForwarder.reRevert();
+		}
+	}
 }
